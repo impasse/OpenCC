12fedd9e1ee3387f5e21e688857a35172a0c7340
shyling <i@uuz.io>
2019-08-13 19:26:13 +0800

    Add jni

---
+++ src/tools/OpenCC_Jni.cpp
@@ -0,0 +1,59 @@
+#include "../opencc.h"
+#include "jni.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+
+const char* className = "com/mokahr/medivh/engine/OpenCC";
+
+class OpenCC {
+private:
+  opencc_t instance;
+
+public:
+  OpenCC(const char* configFileName) { instance = opencc_open(configFileName); }
+
+  ~OpenCC() { opencc_close(instance); }
+
+  string convert(const char* input, size_t length) {
+    char* tmp = opencc_convert_utf8(instance, input, length);
+    string str = string(tmp);
+    opencc_convert_utf8_free(tmp);
+    return str;
+  }
+};
+
+
+jstring convert(JNIEnv* env, jobject obj, jstring config, jstring input) {
+  jboolean isCopy = JNI_FALSE;
+  const char* ip = env->GetStringUTFChars(input, &isCopy);
+  const char* cf = env->GetStringUTFChars(config, &isCopy);
+  jsize len = env->GetStringUTFLength(input);
+  string result = OpenCC(cf).convert(ip, len);
+  env->ReleaseStringUTFChars(config, cf);
+  env->ReleaseStringUTFChars(input, ip);
+  return env->NewStringUTF(result.c_str());
+}
+
+static JNINativeMethod methods[] = {{"convert",
+                                     "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
+                                     reinterpret_cast<void*>(convert)}};
+
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+  JNIEnv* env = NULL;
+  if (JNI_OK != vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_4)) {
+    std::cout << "JNI_OnLoad could not get JNI env" << std::endl;
+    return JNI_ERR;
+  }
+
+  jclass clazz = env->FindClass(className);
+
+  if (env->RegisterNatives(clazz, methods,
+                           sizeof(methods) / sizeof((methods)[0])) < 0) {
+    std::cout << "RegisterNatives error" << std::endl;
+    return JNI_ERR;
+  }
+
+  return JNI_VERSION_1_4;
+}
